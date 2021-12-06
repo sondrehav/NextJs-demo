385e0329ab82b2ea145627fe6d6592f02b5bdf70
Sondre <sondre.havellen@gmail.com>
2021-12-06 23:12:51 +0100

    build fix

--- src/components/contentTransformer.tsx
+++ src/components/contentTransformer.tsx
@@ -5,18 +5,32 @@ import {
 } from "@crystallize/react-content-transformer";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { dracula as theme } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import Link from "next/link";
 
 export type ContentTransformerProps = CProps;
 
+const LinkContent = (props: NodeProps) => {
+  if(!props.metadata) return null;
+  if((props.metadata.href as string).startsWith("/")) {
+    return <Link href={props.metadata.href}>
+      <a className={"border-b-2 border-gray-300 transition-all hover:border-gray-100"}>
+        <NodeContent {...props} />
+      </a>;
+    </Link>
+  }
+  return <a href={props.metadata.href} className={"border-b-2 border-gray-300 transition-all hover:border-gray-100"}><NodeContent {...props} /></a>;
+};
+const QuoteContent = (props: NodeProps) => <p className={"border-l-2 px-4 my-4 border-gray-300 "}><i><NodeContent {...props} /></i></p>;
+const ParagraphContent = (props: NodeProps) => <p className={"my-4"}><NodeContent {...props} /></p>;
+const CodeContent = (props: NodeProps) => <SyntaxHighlighter showLineNumbers language="tsx" style={theme}>
+  {props.children?.filter(s => s.type !== "line-break").map(c => c.textContent).join("\n") ?? ""}
+</SyntaxHighlighter>;
+
 const overrides: Overrides = {
-  link: (props: NodeProps) => <a href={props.metadata!.href} className={"border-b-2 border-gray-300 transition-all hover:border-gray-100"}><NodeContent {...props} /></a>,
-  quote: (props: NodeProps) => <p className={"border-l-2 px-4 my-4 border-gray-300 "}><i><NodeContent {...props} /></i></p>,
-  paragraph: (props: NodeProps) => <p className={"my-4"}><NodeContent {...props} /></p>,
-  code: (props: NodeProps) => <>
-    <SyntaxHighlighter showLineNumbers language="tsx" style={theme}>
-      {props.children?.filter(s => s.type !== "line-break").map(c => c.textContent).join("\n") ?? ""}
-    </SyntaxHighlighter>
-  </>
+  link: LinkContent,
+  quote: QuoteContent,
+  paragraph: ParagraphContent,
+  code: CodeContent
 };
 
 const ContentTransformer = (props: ContentTransformerProps) => {
