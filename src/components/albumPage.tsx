9d67d147581d532d2be9c0622c2b0196fcd1fcb6
Sondre <sondre.havellen@gmail.com>
2021-12-07 23:03:29 +0100

    Navbar fixes and moved createdOn

--- src/components/albumPage.tsx
+++ src/components/albumPage.tsx
@@ -25,12 +25,14 @@ const AlbumPage = (item: Item) => {
   return (<Layout item={item}>
 
     <div className={classNames(container, "my-6 overflow-x-hidden space-y-4")}>
-      <div className={"flex flex-row justify-between items-center my-2"}>
-        {title && <h1 className={"text-3xl"}>{title}</h1>}
-        {item.createdAt && <span className={"opacity-50"}><i>Opprettet <DateComponent dateString={item.createdAt}/></i></span>}
-      </div>
+      {title && <h1 className={"text-3xl"}>{title}</h1>}
       {authors && <div className={"grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 md:gap-4"}>
         {authors.map((author, i) => <ArticleProfileTag key={i} item={author}/>)}
+      </div>}
+      {item.createdAt && <div className={"opacity-50"}>
+        <span className={"my-4"}>
+          <i>Opprettet <DateComponent dateString={item.createdAt}/></i>
+        </span>
       </div>}
     </div>
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -39,38 +39,41 @@ const Navbar = ({ title, links }: { title: string, links: Item[] }) => {
   return (<nav className="sticky bg-gray-900 top-0 z-30 w-full shadow-lg border-b border-gray-800">
     <div className={classNames(container, "relative px-0 z-50")}>
       <div className="relative flex items-center justify-between h-16">
-        <div className="absolute flex-grow-0 flex-shrink-0 inset-y-0 left-0 flex items-center sm:hidden">
-          <button type="button"
-                  className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                  aria-controls="mobile-menu" aria-expanded="false"
-                  onClick={() => setExpanded(!expanded)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor" aria-hidden="true">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
-            </svg>
-            <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor" aria-hidden="true">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
-            </svg>
-          </button>
-        </div>
-        <div className="w-full relative overflow-x-auto flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-          <div className="hidden sm:block">
-            <div className="flex space-x-4 text-center items-center whitespace-nowrap">
-              {links.map(link => <LinkItem key={link.path} link={link}/>)}
+
+        <button type="button"
+                className="sm:hidden inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu" aria-expanded="false"
+                onClick={() => setExpanded(!expanded)}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+               stroke="currentColor" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
+          </svg>
+          <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+               stroke="currentColor" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
+          </svg>
+        </button>
+
+        <div className={"flex-grow-1 flex-shrink-1 flex justify-between items-center max-w-full overflow-x-hidden truncate"}>
+
+            <div className="hidden sm:block flex-grow-0 flex-shrink-1 relative overflow-x-auto flex space-x-4 text-center items-center whitespace-nowrap h-full">
+              <div className="flex space-x-4 text-center items-center whitespace-nowrap w-full h-full">
+                {links.map(link => <LinkItem key={link.path} link={link}/>)}
+              </div>
             </div>
-          </div>
+
+            <h2 className={"text-lg flex-grow-1 flex-shrink-1 sm:flex-grow-0 sm:flex-shrink-0 overflow-x-hidden truncate ml-0 sm:ml-2"} title={title}>
+              {title}
+            </h2>
+
         </div>
-        <div className={"flex-grow-0 flex-shrink-0 flex flex-row items-center space-x-4 pl-4"}>
-          <h2 className={"text-lg"}>
-            {title}
-          </h2>
-          <a href={"https://github.com/sondrehav/NextJs-demo"}>
-            <GithubSquare className={"fill-current transform transition-all scale-90 hover:scale-100 cursor-pointer"} width={32} height={32}/>
-          </a>
-        </div>
+
+        <a className={"flex-grow-0 flex-shrink-0 p-2"} href={"https://github.com/sondrehav/NextJs-demo"}>
+          <GithubSquare className={"fill-current transform transition-all scale-90 hover:scale-100 cursor-pointer"} width={32} height={32}/>
+        </a>
+
       </div>
     </div>
 
