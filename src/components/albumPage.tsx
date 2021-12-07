00633ecf255b11d9e6fb0286536c7833ecdbd22d
Sondre <sondre.havellen@gmail.com>
2021-12-07 12:00:34 +0100

    Related articles

--- src/components/albumPage.tsx
+++ src/components/albumPage.tsx
@@ -1,11 +1,20 @@
 import Layout from "components/layout";
 import findComponent from "lib/findComponent";
-import {ComponentType, ImageContent, Item, RichTextContent, SingleLineContent} from "types/catalogue";
+import {
+  ComponentType,
+  ImageContent,
+  Item,
+  ItemRelationsContent,
+  RichTextContent,
+  SingleLineContent
+} from "types/catalogue";
 import DateComponent from "components/date";
 import ContentTransformer, {ContentTransformerProps} from "components/contentTransformer";
 import ImageView from "components/imageView";
 import classNames from "classnames";
 import {container} from "lib/classes";
+import Link from "next/link";
+import ChildPageView from "components/childPageView";
 
 const AlbumPage = (item: Item) => {
 
@@ -14,6 +23,7 @@ const AlbumPage = (item: Item) => {
   const images = findComponent<ImageContent>(item.components ?? [], ComponentType.Images, "images")?.images;
   const author = findComponent<SingleLineContent>(item.components ?? [], ComponentType.SingleLine, "author")?.text;
   const authorContact = findComponent<SingleLineContent>(item.components ?? [], ComponentType.SingleLine, "author-contact")?.text;
+  const relatedArticles = findComponent<ItemRelationsContent>(item.components ?? [], ComponentType.ItemRelations, "related-articles")?.items;
 
   return (<Layout item={item}>
     <div className={classNames(container, "my-6 overflow-x-hidden")}>
@@ -26,6 +36,14 @@ const AlbumPage = (item: Item) => {
       {title && <h1 className={"text-3xl my-8"}>{title}</h1>}
       {content && <ContentTransformer json={content as ContentTransformerProps["json"]}/>}
     </div>
+
+    {relatedArticles && relatedArticles?.length > 0 && <>
+    {title && <h2 className={classNames(container, "text-2xl my-8")}>Relaterte artikler</h2>}
+    <div className={classNames(container, "my-6 overflow-x-hidden grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 md:gap-8")}>
+      {relatedArticles.map(child => <Link key={child.id} href={child.path!.replace("/web", "")} passHref><ChildPageView child={child}/></Link>)}
+    </div>
+  </>}
+
   </Layout>)
 
 };
